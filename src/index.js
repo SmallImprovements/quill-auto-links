0149426167df2da9a50df81568577d513eb41131
Remco Haszing <remcohaszing@gmail.com>
2017-11-23 11:57:20 +0100

    feat(*) add support for email links

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const DEFAULT_OPTIONS = {
 
 const REGEXP_HTTP_GLOBAL = /https?:\/\/[^\s]+/g;
 const REGEXP_HTTP_WITH_PRECEDING_WS = /(?:\s|^)(https?:\/\/[^\s]+)/;
+// Email regexp shamelessly copied from https://github.com/epoberezkin/ajv/blob/v6.0.0-beta.2/lib/compile/formats.js#L46
+const REGEXP_EMAIL_GLOBAL = /[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)[^\s]+/g;
+const REGEXP_EMAIL_WITH_PRECEDING_WS = /(?:\s|^)([a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)[^\s]+)/;
 
 const sliceFromLastWhitespace = (str) => {
   const whitespaceI = str.lastIndexOf(' ');
@@ -28,6 +31,21 @@ function registerTypeListener(quill) {
       return true;
     }
   });
+  quill.keyboard.addBinding({
+    collapsed: true,
+    key: ' ',
+    prefix: REGEXP_EMAIL_WITH_PRECEDING_WS,
+    handler: (range, context) => {
+      const url = sliceFromLastWhitespace(context.prefix);
+      const ops = [
+          { retain: range.index - url.length },
+          { 'delete': url.length },
+          { insert: url, attributes: { link: 'mailto:' + url } }
+      ];
+      quill.updateContents({ ops });
+      return true;
+    }
+  });
 }
 
 function registerPasteListener(quill) {
@@ -44,6 +62,27 @@ function registerPasteListener(quill) {
         const beforeLink = split.shift();
         ops.push({ insert: beforeLink });
         ops.push({ insert: match, attributes: { link: match } });
+        str = split.join(match);
+      });
+      ops.push({ insert: str });
+      delta.ops = ops;
+    }
+
+    return delta;
+  });
+  quill.clipboard.addMatcher(Node.TEXT_NODE, (node, delta) => {
+    if (typeof node.data !== 'string') {
+      return;
+    }
+    const matches = node.data.match(REGEXP_EMAIL_GLOBAL);
+    if (matches && matches.length > 0) {
+      const ops = [];
+      let str = node.data;
+      matches.forEach(match => {
+        const split = str.split(match);
+        const beforeLink = split.shift();
+        ops.push({ insert: beforeLink });
+        ops.push({ insert: match, attributes: { link: 'mailto:' + match } });
         str = split.join(match);
       });
       ops.push({ insert: str });
